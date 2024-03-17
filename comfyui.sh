b5311dada88ef001e0a1e8cd155121376663cb8e
suaidishak <suaid.ishak@gmail.com>
2024-03-17 11:10:42 +0800

    Update comfyui.sh

--- comfyui.sh
+++ comfyui.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get install libgoogle-perftools-dev
+sudo apt-get install libgoogle-perftools-dev -y
 sudo snap install ngrok
 ngrok authtoken 2YmLJMmvcUf0UzgqM7Mtk9oRhav_88jaJRR3wnBTff9diyZnz
 git clone https://github.com/comfyanonymous/ComfyUI.git
